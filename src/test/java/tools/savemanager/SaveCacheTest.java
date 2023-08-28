82e36e7a235b28e839e749c6d2cb803fa94a1bab
BuchananDJB <buchanandjb@gmail.com>
2023-08-27 23:07:48 -0600

    Added unit tests for SaveCache

---
+++ src/test/java/tools/savemanager/SaveCacheTest.java
@@ -0,0 +1,50 @@
+package tools.savemanager;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Mockito.*;
+
+public class SaveCacheTest {
+
+    private SaveCache saveCache;
+
+    @Before
+    public void before() {
+        saveCache = SaveCache.getInstance();
+    }
+
+    @Test
+    public void addSaveItem_test() {
+        SaveItem mockSaveItem = mock(SaveItem.class);
+        assertTrue(saveCache.addSaveItem(mockSaveItem));
+        assertFalse(saveCache.addSaveItem(mockSaveItem));
+    }
+
+    @Test
+    public void removeSaveItem_test() {
+        SaveItem mockSaveItem = mock(SaveItem.class);
+        assertFalse(saveCache.removeSaveItem(mockSaveItem));
+        assertTrue(saveCache.addSaveItem(mockSaveItem));
+        assertTrue(saveCache.removeSaveItem(mockSaveItem));
+    }
+
+    @Test
+    public void saveAllItems_test() {
+        Set<SaveItem> mockSaveItems = new HashSet<>();
+        for (int i = 0; i < 1000; ++i) {
+            mockSaveItems.add(mock(SaveItem.class));
+        }
+
+        mockSaveItems.forEach(saveItem -> assertTrue(saveCache.addSaveItem(saveItem)));
+        saveCache.saveAllItems();
+        mockSaveItems.forEach(saveItem -> verify(saveItem, times(1)).saveData());
+    }
+
+}
