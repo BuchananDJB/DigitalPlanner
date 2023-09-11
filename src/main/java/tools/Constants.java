ab1488d7776edd9049b15de2ef6a612eaa5e73e6
BuchananDJB <buchanandjb@gmail.com>
2023-09-10 18:03:12 -0600

    handle error when can't locate icon files

--- src/main/java/tools/Constants.java
+++ src/main/java/tools/Constants.java
@@ -3,7 +3,6 @@ package tools;
 public class Constants {
     public static final String PLANNER_ROOT_DIRECTORY = "Planner/";
     public static final String TABVIEWS_DIRECTORY = PLANNER_ROOT_DIRECTORY + "TabViews/";
-    public static final String PREFERENCES_FILE_PATH = PLANNER_ROOT_DIRECTORY + "preferences.txt";
 
     public static final String DAILY_INFO = "DailyInfo/";
 
@@ -11,4 +10,8 @@ public class Constants {
     public static final String GENERAL_TODO_INCOMPLETE = "General-Incomplete.json";
     public static final String SOCIAL_TODO_COMPLETE = "Social-Complete.json";
     public static final String SOCIAL_TODO_INCOMPLETE = "Social-Incomplete.json";
+
+    public static final String PREFERENCES_FILE_PATH = PLANNER_ROOT_DIRECTORY + "preferences.txt";
+    public static final String LARGE_ICON_FILE_PATH = "resources/icons/CalendarIcon_large.png";
+    public static final String SMALL_ICON_FILE_PATH = "resources/icons/CalendarIcon_small.png";
 }
